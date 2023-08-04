ffc790de2be980b918a567511d136a23e086dece
telus7 <mikolaj.telus@gmail.com>
2023-08-04 14:25:04 +0200

    add login and registration erros handling

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,62 +6,59 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
-  
+export class LoginComponent implements OnInit {
+  errorMessage: string = '';
+
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required]]
-});
+    password: ['', [Validators.required]],
+  });
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
-  }
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
 
-  
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   isTouched(controlName: string) {
     const control = this.loginForm.get(controlName);
     return control && control.touched;
   }
-  
+
   hasError(controlName: string, errorName: string) {
     const control = this.loginForm.get(controlName);
     return control && control.hasError(errorName);
   }
 
   onLogin() {
-    console.log('onLogin');
     if (this.loginForm.valid) {
-      console.log('valid');
       const emailControl = this.loginForm.get('email');
       const passwordControl = this.loginForm.get('password');
 
-      if (emailControl && passwordControl && emailControl.value && passwordControl.value) {
-          const request: UserLoginRequest = {
-              email: emailControl.value,
-              password: passwordControl.value
+      if (
+        emailControl &&
+        passwordControl &&
+        emailControl.value &&
+        passwordControl.value
+      ) {
+        const request: UserLoginRequest = {
+          email: emailControl.value,
+          password: passwordControl.value,
+        };
+
+        this.authService.loginUser(request).subscribe(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.log(error);
+            this.errorMessage = error;
           }
-        
+        );
+      } else {
 
-
-      this.authService.loginUser(request).subscribe(
-        response => {
-          console.log(response);
-        },
-        error => {
-          console.log(error);
-        }
-      )
-    } else {
-      console.log('invalid  ttr');
-
-      this.loginForm.markAllAsTouched();
+        this.loginForm.markAllAsTouched();
+      }
     }
   }
-  }
-
 }
